5156a24bf4479baa6a4e946d68df44aa9fd1a46b
dancan-agweli <agwelidanan32@gmail.com>
2023-08-29 04:00:04 -0700

    configuaration

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { '/tmp/school':
-  ensure   => 'file',
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
-}
+# create a file in /tmp with specific requirements
 
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
